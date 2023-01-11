96d2ac1565bfb625fa3f65694ad038dd365e9f32
Dmitry Kanaev <dimmkan@mail.ru>
2023-01-11 08:17:06 +0300

    Select from DB

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module golang-pubsub
 
 go 1.19
 
-require github.com/go-redis/redis v6.15.9+incompatible
+require (
+	github.com/go-redis/redis v6.15.9+incompatible
+	github.com/lib/pq v1.10.7
+)
--- main.go
+++ main.go
@@ -1,13 +1,22 @@
 package main
 
 import (
+	"database/sql"
 	"encoding/json"
 	"fmt"
 	"log"
 	"time"
 
 	"github.com/go-redis/redis"
+	_ "github.com/lib/pq"
 )
+
+type User struct {
+	ID        int    `json:"id"`
+	Email     string `json:"email"`
+	Phone     string `json:"phone"`
+	Timestamp string `json:"timestamp"`
+}
 
 func main() {
 	// Connect to Redis server
@@ -17,12 +26,18 @@ func main() {
 		DB:       0,
 	})
 
+	connStr := "user=postgres dbname=backend_my_house sslmode=disable"
+	db, err := sql.Open("postgres", connStr)
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
 	// There is no error because go-redis automatically reconnects on error.
 	pubsub := rdb.Subscribe("myCoolChannel1")
 
 	// Close the subscription when we are done.
 	defer pubsub.Close()
-
 	ch := pubsub.Channel()
 
 	var loop chan struct{}
@@ -35,14 +50,30 @@ func main() {
 				fmt.Println(err)
 			}
 
-			data["timestamp"] = time.Now().Format(time.RFC1123)
+			selectString := fmt.Sprintf("SELECT id, email, phone FROM users WHERE id = %v", data["user_id"])
+			rows, err := db.Query(selectString)
+			if err != nil {
+				panic(err)
+			}
 
-			b, err := json.Marshal(data)
+			users := []User{}
+			for rows.Next() {
+				user := User{}
+				err := rows.Scan(&user.ID, &user.Email, &user.Phone)
+				if err != nil {
+					panic(err)
+				}
+				user.Timestamp = time.Now().Format(time.RFC1123)
+				users = append(users, user)
+			}
+
+			b, err := json.Marshal(users)
 			if err != nil {
 				fmt.Println(err)
 			}
 
 			rdb.Publish("myCoolChannel2", b)
+			rows.Close()
 		}
 	}()
 
